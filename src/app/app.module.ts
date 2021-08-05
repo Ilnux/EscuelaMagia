321b96b113539d065e383c1a37dc70ecd6c84c42
Felipe <felipe-0210@outlook.com>
2021-08-05 11:04:09 -0500

    configuracion de segundo modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,23 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import {APP_ROUTING} from "./app.rutas";
 
 //componentes
 import {AppComponent} from './app.component';
-import {InicioComponent} from './componentes/inicio/inicio.component';
-import {TarjetaCasaComponent} from './componentes/tarjeta-casa/tarjeta-casa.component';
-import {TarjetaMiembroComponent} from './componentes/tarjeta-miembros/tarjeta-miembro.component';
 
-import {APP_ROUTING} from "./app.rutas";
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
-
-
-
+//modulos
+import {InfoEscuelasModule} from "./modulos/info-escuelas/info-escuelas.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InicioComponent,
-    TarjetaCasaComponent,
-    TarjetaMiembroComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING,
-    HttpClientModule,
     BrowserAnimationsModule,
-    FormsModule
+    InfoEscuelasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modulos/info-escuelas/info-escuelas.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+//componentes
+import {InicioComponent} from '../../componentes/inicio/inicio.component';
+import {TarjetaCasaComponent} from '../../componentes/tarjeta-casa/tarjeta-casa.component';
+import {TarjetaMiembroComponent} from '../../componentes/tarjeta-miembros/tarjeta-miembro.component';
+
+//servicio
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+
+
+@NgModule({
+  declarations: [
+    InicioComponent,
+    TarjetaCasaComponent,
+    TarjetaMiembroComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule
+  ]
+})
+export class InfoEscuelasModule {
+}
